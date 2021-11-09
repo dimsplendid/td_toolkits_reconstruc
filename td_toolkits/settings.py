fd3d33c09abf845b566cba5bbb4d61e1d0cdfd46
andrew.tseng 曾煒翔 <andrew.tseng@cminl.oa>
2021-11-09 16:59:41 +0800

    create new app: tr2calculator

--- td_toolkits/settings.py
+++ td_toolkits/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Add our new application
+    'tr2calculator.apps.Tr2CalculatorConfig',
 ]
 
 MIDDLEWARE = [
--- tr2calculator/models.py
+++ tr2calculator/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
+# > python manage.py makemigrations
+# > python manage.py migrate
+# Warning: You'll need to run these commands every time 
+# your models change in a way that will affect the structure of 
+# the data that needs to be stored 
+# (including both addition and removal of whole models and individual fields).
+
+
 # Create your models here.
