aec632c15d072ba4dbb1f7604a7b0356cb7bbc19
dimsplendid <dimsplendid@gmail.com>
2021-12-07 15:12:57 +0800

    demo 20211208

--- ra_exploer/models.py
+++ ra_exploer/models.py
@@ -6,45 +6,6 @@ from materials.models import LiquidCrystal, Polyimide, Seal
 
 # Create your models here.
 
-
-# class LiquidCrystal(models.Model):
-#     """Model record LC"""
-#     name = models.CharField(max_length=20, unique=True,
-#                             help_text='Enter a LC name')
-#     vender = ForeignKey('Vender', on_delete=models.RESTRICT,
-#                         null=True, blank=True)
-
-#     class META:
-#         ordering = ['name']
-
-#     def __str__(self):
-#         return self.name
-
-
-# class Polyimide(models.Model):
-#     """Model record PI"""
-#     name = models.CharField(max_length=20, unique=True,
-#                             help_text='Enter a PI name')
-
-#     class META:
-#         ordering = ['name']
-
-#     def __str__(self):
-#         return self.name
-
-
-# class Seal(models.Model):
-#     """Model record Seal"""
-#     name = models.CharField(max_length=20, unique=True,
-#                             help_text='Enter a Seal name')
-
-#     class META:
-#         ordering = ['name']
-
-#     def __str__(self):
-#         return self.name
-
-
 class Vender(models.Model):
     """Model record Vender"""
     name = models.CharField(max_length=200, unique=True,
